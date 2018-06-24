2cc18779bf4a52965153e9940ae0b056084ba97d
Gedeon kas <kas.gedeon20@gmail.com>
2018-06-24 17:33:47 +0300

    Commit One

---
+++ src/implement_Login/LoginImplement.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package implement_Login;
+
+/**
+ *
+ * @author Daniel
+ */
+
+import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
+import rmi_login.interface_Login.LoginInterface;
+
+public class LoginImplement extends UnicastRemoteObject implements LoginInterface {
+
+    public LoginImplement() throws RemoteException {
+                
+    }
+    
+    //@Override
+    public boolean getLogin(String user, String pass) throws RemoteException {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+        boolean found = false;
+        try {
+            if(user.equals("admin") && pass.equals("1234")) {
+                found = true;
+                return found;
+            } else {
+                found = false;
+                return found;
+            }
+            
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return found;
+    }
+}
